161e53d1a3a16606bda5c95511871494825c0257
Ivan Klivitskyi <vklivitskiy@gmail.com>
2025-05-31 13:11:16 +0200

    updated

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -3,5 +3,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: 'cypress-test/support/e2e.js',
+    specPattern: 'cypress-test/e2e/**/*.cy.{js,jsx,ts,tsx}'
   }
 });
